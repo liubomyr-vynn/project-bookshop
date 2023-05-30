7bd1a60e7e35bbf69226401f18284d089fb2ef14
liubomyr-vynn <pro.vynn.l@gmail.com>
2023-05-30 11:04:33 +0300

    Update slider-foundation.js

--- src/js/slider-foundation.js
+++ src/js/slider-foundation.js
@@ -1,24 +1,20 @@
-// import { markup } from './markup-foundation'
-
-
 const sliderBtnEl = document.querySelector('.foundation__btn');
 const foundItemsEl = document.querySelectorAll('.foundation__item');
-const foundationListEl = document.querySelector(".foundation__list");
+const foundationListEl = document.querySelector('.foundation__list');
 
 // SLIDER
 const downSlider = function () {
   foundItemsEl.forEach(function (slide) {
-        slide.style.transform = `translateY(-${counter * 90}px)`;
-        // slide.style.transform = `translateY(-${counter * 80}px)`;
+    slide.style.transform = `translateY(-${counter * 90}px)`;
   });
-}
+};
 
 const upSlider = function () {
   foundItemsEl.forEach(function (slide) {
     slide.style.transform = `translateY(0px)`;
-    sliderBtnEl.querySelector('.btnLoadMore__icon').style.transform = '';  });
-}
-
+    sliderBtnEl.querySelector('.btnLoadMore__icon').style.transform = '';
+  });
+};
 
 // BUTTON CLICK
 sliderBtnEl.addEventListener('click', movementSlide);
@@ -26,7 +22,7 @@ sliderBtnEl.addEventListener('click', movementSlide);
 let counter = 0;
 let down = false;
 
- function movementSlide () {
+function movementSlide() {
   if (down) {
     if (counter < 2) {
       counter = 1;
@@ -35,16 +31,14 @@ let down = false;
   } else {
     counter += 1;
     if (counter === 2) {
-      sliderBtnEl.querySelector('.btnLoadMore__icon').style.transform = 'rotate(270deg)';
+      sliderBtnEl.querySelector('.btnLoadMore__icon').style.transform =
+        'rotate(270deg)';
     }
-        if (counter > 2) {
-        counter = +1;
-        down = true;       
-        return upSlider();
+    if (counter > 2) {
+      counter = +1;
+      down = true;
+      return upSlider();
     }
-    
-   } 
-   downSlider();
+  }
+  downSlider();
 }
-
-
