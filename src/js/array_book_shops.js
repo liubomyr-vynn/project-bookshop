e63a715573a92eb67b1255a120f952045b860c44
liubomyr-vynn <pro.vynn.l@gmail.com>
2023-05-30 11:04:10 +0300

    Update array_book_shops.js

--- src/js/array_book_shops.js
+++ src/js/array_book_shops.js
@@ -1,8 +1,7 @@
 export const bookShops = {
   amazon: new URL('../images/shop-icons/amazon.jpg', import.meta.url),
   bookshop: new URL('../images/shop-icons/bookstore.jpg', import.meta.url),
-  openbook: new URL('../images/shop-icons/applebooks.jpg', import.meta.url),  
+  openbook: new URL('../images/shop-icons/applebooks.jpg', import.meta.url),
   dump: new URL('../image/shoppinglist/dump.png', import.meta.url),
   emptypage: new URL('../image/shoppinglist/emptypage.png', import.meta.url),
-}
-
+};
